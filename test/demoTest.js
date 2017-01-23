a9c01ce49c8880186b4d89e4e7f2d424b970d6a7
Devon Hogencamp <devhogen1@gmail.com>
2017-01-23 13:01:42 -0700

    intro to mocha testing

---
+++ test/demoTest.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+/*
+    Just a demo test for mocha
+*/
+
+describe('Some demo tests', function () {
+    // Create tests
+    it('adds 2 numbers together', function () {
+        assert(2 + 3 === 5);
+    });
+});
