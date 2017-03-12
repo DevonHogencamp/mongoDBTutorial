6c8b5ff565db54b165f89203ab90ae3b475c9032
Devon Hogencamp <devhogen1@gmail.com>
2017-03-12 16:12:44 -0700

    Saved mario char to db

--- test/savingTest.js
+++ test/savingTest.js
@@ -1,11 +1,28 @@
 var assert = require('assert');
+
+// Bring in the MarioChar schema
+var MarioChar = require('../models/mariochar');
 /*
     Just a demo test for mocha
 */
 
-describe('Some demo tests', function () {
+describe('Saving records', function () {
     // Create tests
-    it('adds 2 numbers together', function () {
-        assert(2 + 3 === 5);
+    // done is used to tell mocha we are done with our test in async code
+    it('Saves a record to the database', function (done) {
+        // Create an instance of the MarioChar schema
+        var char = new MarioChar({
+            name: "Mario",
+            weight: 150
+        });
+
+        // Save that instance, then when that finishes
+        char.save().then(function () {
+            // Checks to see if the char is put into the db
+            assert(char.isNew === false);
+
+            // Tell mocha we are done
+            done();
+        });
     });
 });
