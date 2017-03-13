e64a9355b0635c011f417bf37f6364afb79b01d5
Devon Hogencamp <devhogen1@gmail.com>
2017-03-12 17:44:57 -0700

    Make mocha wait till connection has been made before running tests

--- test/connection.js
+++ test/connection.js
@@ -4,11 +4,16 @@ var mongoose = require('mongoose');
 // ES6 Promises
 mongoose.Promise = global.Promise;
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/testaroo');
+// Connect to the DB before tests run
+before(function (done) {
+    // Connect to MongoDB
+    mongoose.connect('mongodb://localhost/testaroo');
 
-mongoose.connection.once('open', function () {
-    console.log('Connection has been made!');
-}).on('error', function (err) {
-    console.log('Connection error: ' + err);
+    mongoose.connection.once('open', function () {
+        console.log('Connection has been made!');
+
+        done();
+    }).on('error', function (err) {
+        console.log('Connection error: ' + err);
+    });
 });
